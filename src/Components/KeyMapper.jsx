ac137fac8a9e5b4288134ac3f572fcadf3fa2eb5
Tim D'Arcy <trdarcy@outlook.com>
2020-01-23 17:11:23 +1000

    Prints key code but unsure how to implement keboardEvent.code

--- src/Components/KeyMapper.jsx
+++ src/Components/KeyMapper.jsx
@@ -4,25 +4,36 @@ import React from 'react'
 export default class KeyMapper extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-            keyPress: ''
-        }
+        this.state = {}
     }
     render(){
         return(
             <>
-                <input 
+                <div 
                     type="text"
-                    onKeyDown={(keyDown) => {
-                        
-                        this.setState({keyPress: keyDown}, () => {
-                            console.log(this.state.keyPress)
+                    className="main-display"
+                    tabIndex="0"
+                    
+                    onKeyDownCapture={(keyDown) => {
+                        keyDown.preventDefault()
+                        this.setState({
+                            keyPress: keyDown.key,
+                            keyCode: keyDown.keyCode,
+                            keyLocation: keyDown.location,
+                            keyWhich: keyDown.which
+                        }, () => {
+                            console.log(keyDown.persist())
                         })
-                        
-                        
                     }}
-                />
-                <div>{this.state.keyPress}</div>
+                    >
+                        <ul>
+                            <li>KeyPress: {this.state.keyPress}</li>
+                            <li>KeyCode: {this.state.keyCode}</li>
+                            <li>KeyLocation: {this.state.keyLocation}</li>
+                            <li>KeyWhich: {this.state.keyWhich}</li>
+                        </ul>
+                    </div>
+                
             </>
         )
     }
